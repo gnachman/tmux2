9ad0fcf89b5e7643b13d69fe277b34897864d925
George Nachman <git.xyz@georgester.com>
2011-12-09 09:15:00 -0800

    Output layouts for control clients when resize-pane is run

--- cmd-resize-pane.c
+++ cmd-resize-pane.c
@@ -26,92 +26,97 @@
  * Increase or decrease pane size.
  */
 
-void	cmd_resize_pane_key_binding(struct cmd *, int);
-int	cmd_resize_pane_exec(struct cmd *, struct cmd_ctx *);
+void    cmd_resize_pane_key_binding(struct cmd *, int);
+int cmd_resize_pane_exec(struct cmd *, struct cmd_ctx *);
 
 const struct cmd_entry cmd_resize_pane_entry = {
-	"resize-pane", "resizep",
-	"DLRt:U", 0, 1,
-	"[-DLRU] " CMD_TARGET_PANE_USAGE " [adjustment]",
-	0,
-	cmd_resize_pane_key_binding,
-	NULL,
-	cmd_resize_pane_exec
+    "resize-pane", "resizep",
+    "DLRt:U", 0, 1,
+    "[-DLRU] " CMD_TARGET_PANE_USAGE " [adjustment]",
+    0,
+    cmd_resize_pane_key_binding,
+    NULL,
+    cmd_resize_pane_exec
 };
 
 void
 cmd_resize_pane_key_binding(struct cmd *self, int key)
 {
-	switch (key) {
-	case KEYC_UP | KEYC_CTRL:
-		self->args = args_create(0);
-		args_set(self->args, 'U', NULL);
-		break;
-	case KEYC_DOWN | KEYC_CTRL:
-		self->args = args_create(0);
-		args_set(self->args, 'D', NULL);
-		break;
-	case KEYC_LEFT | KEYC_CTRL:
-		self->args = args_create(0);
-		args_set(self->args, 'L', NULL);
-		break;
-	case KEYC_RIGHT | KEYC_CTRL:
-		self->args = args_create(0);
-		args_set(self->args, 'R', NULL);
-		break;
-	case KEYC_UP | KEYC_ESCAPE:
-		self->args = args_create(1, "5");
-		args_set(self->args, 'U', NULL);
-		break;
-	case KEYC_DOWN | KEYC_ESCAPE:
-		self->args = args_create(1, "5");
-		args_set(self->args, 'D', NULL);
-		break;
-	case KEYC_LEFT | KEYC_ESCAPE:
-		self->args = args_create(1, "5");
-		args_set(self->args, 'L', NULL);
-		break;
-	case KEYC_RIGHT | KEYC_ESCAPE:
-		self->args = args_create(1, "5");
-		args_set(self->args, 'R', NULL);
-		break;
-	default:
-		self->args = args_create(0);
-		break;
-	}
+    switch (key) {
+    case KEYC_UP | KEYC_CTRL:
+        self->args = args_create(0);
+        args_set(self->args, 'U', NULL);
+        break;
+    case KEYC_DOWN | KEYC_CTRL:
+        self->args = args_create(0);
+        args_set(self->args, 'D', NULL);
+        break;
+    case KEYC_LEFT | KEYC_CTRL:
+        self->args = args_create(0);
+        args_set(self->args, 'L', NULL);
+        break;
+    case KEYC_RIGHT | KEYC_CTRL:
+        self->args = args_create(0);
+        args_set(self->args, 'R', NULL);
+        break;
+    case KEYC_UP | KEYC_ESCAPE:
+        self->args = args_create(1, "5");
+        args_set(self->args, 'U', NULL);
+        break;
+    case KEYC_DOWN | KEYC_ESCAPE:
+        self->args = args_create(1, "5");
+        args_set(self->args, 'D', NULL);
+        break;
+    case KEYC_LEFT | KEYC_ESCAPE:
+        self->args = args_create(1, "5");
+        args_set(self->args, 'L', NULL);
+        break;
+    case KEYC_RIGHT | KEYC_ESCAPE:
+        self->args = args_create(1, "5");
+        args_set(self->args, 'R', NULL);
+        break;
+    default:
+        self->args = args_create(0);
+        break;
+    }
 }
 
 int
 cmd_resize_pane_exec(struct cmd *self, struct cmd_ctx *ctx)
 {
-	struct args		*args = self->args;
-	struct winlink		*wl;
-	const char	       	*errstr;
-	struct window_pane	*wp;
-	u_int			 adjust;
+    struct args         *args = self->args;
+    struct winlink      *wl;
+    const char          *errstr;
+    struct window_pane  *wp;
+    struct client       *c;
+    u_int                adjust;
 
-	if ((wl = cmd_find_pane(ctx, args_get(args, 't'), NULL, &wp)) == NULL)
-		return (-1);
+    if ((wl = cmd_find_pane(ctx, args_get(args, 't'), NULL, &wp)) == NULL)
+        return (-1);
 
-	if (args->argc == 0)
-		adjust = 1;
-	else {
-		adjust = strtonum(args->argv[0], 1, INT_MAX, &errstr);
-		if (errstr != NULL) {
-			ctx->error(ctx, "adjustment %s", errstr);
-			return (-1);
-		}
-	}
+    if (args->argc == 0)
+        adjust = 1;
+    else {
+        adjust = strtonum(args->argv[0], 1, INT_MAX, &errstr);
+        if (errstr != NULL) {
+            ctx->error(ctx, "adjustment %s", errstr);
+            return (-1);
+        }
+    }
 
-	if (args_has(self->args, 'L'))
-		layout_resize_pane(wp, LAYOUT_LEFTRIGHT, -adjust);
-	else if (args_has(self->args, 'R'))
-		layout_resize_pane(wp, LAYOUT_LEFTRIGHT, adjust);
-	else if (args_has(self->args, 'U'))
-		layout_resize_pane(wp, LAYOUT_TOPBOTTOM, -adjust);
-	else if (args_has(self->args, 'D'))
-		layout_resize_pane(wp, LAYOUT_TOPBOTTOM, adjust);
-	server_redraw_window(wl->window);
+    if (args_has(self->args, 'L'))
+        layout_resize_pane(wp, LAYOUT_LEFTRIGHT, -adjust);
+    else if (args_has(self->args, 'R'))
+        layout_resize_pane(wp, LAYOUT_LEFTRIGHT, adjust);
+    else if (args_has(self->args, 'U'))
+        layout_resize_pane(wp, LAYOUT_TOPBOTTOM, -adjust);
+    else if (args_has(self->args, 'D'))
+        layout_resize_pane(wp, LAYOUT_TOPBOTTOM, adjust);
+    server_redraw_window(wl->window);
 
-	return (0);
+    c = cmd_find_client(ctx, NULL);
+    if (c && (c->flags & CLIENT_CONTROL))
+        control_print_session_layouts(c->session, ctx);
+
+    return (0);
 }
